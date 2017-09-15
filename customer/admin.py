5ab72cb96a037849982e5c3d698fd181a19a8b95
Tahmina Khatoon <tahmina@brainstation-23.com>
2017-09-15 13:22:43 +0000

    project setup

---
+++ customer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Country,Customer
+
+admin.site.register(Country)
+admin.site.register(Customer)
---
+++ customer/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
