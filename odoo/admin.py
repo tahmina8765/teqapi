b496e59e1c5b81c0454c7b7ddf55cacdbdac9504
Shubho <shubho.saha@bs-23.net>
2017-09-16 01:13:55 +0600

    add option modelAdmin for res.users

--- odoo/admin.py
+++ odoo/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import ResUsers
 
-admin.site.register(ResUsers)
+@admin.register(ResUsers)
+class ResUsersModelAdmin(admin.ModelAdmin):
+
+    list_display = ['user_name','mobile','login']
+    list_filter = ['active']
+    search_fields = ['user_name','login','mobile']
+
+    class Meta:
+        model = ResUsers
